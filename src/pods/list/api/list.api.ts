f1466125a493c23222df0b37986ecda21fb43dd5
David Roig <DRoigHeredia@gmail.com>
2022-04-27 20:14:21 +0200

    added backened conection

--- src/pods/list/api/list.api.ts
+++ src/pods/list/api/list.api.ts
@@ -1,11 +1,14 @@
 import Axios from 'axios';
 import { Member } from './list.api-model';
 
+const urlBase = `${process.env.BASE_API_URL}/members`;
 export const getMemberList = async (
   organization: string
 ): Promise<Member[]> => {
-  const { data } = await Axios.get(
-    `https://api.github.com/orgs/${organization}/members`
-  );
+  const { data } = await Axios.get(`${urlBase}/${organization}`, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
   return data;
 };
